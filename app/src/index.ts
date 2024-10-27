134d24222d02a9d05a19379f52d61c4e7487687a
Caleb Hill <chill389cc@gmail.com>
2024-10-27 09:42:05 -0600

    add redirect route for wishlist, /wishlist

--- app/src/index.ts
+++ app/src/index.ts
@@ -24,6 +24,10 @@ async function main (): Promise<void> {
     reply.sendFile('prequels_script.html')
   })
 
+  app.get('/wishlist', function (req: FastifyRequest, reply: FastifyReply) {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    reply.redirect('https://docs.google.com/spreadsheets/d/e/2PACX-1vQExuHz0hIgVbkTxSV_jEMjn3NNcRMWMR9m6ialFSNObfyaM3VEtkjgO30w7ttIe5TtNaMeUF6sjk3J/pubhtml#')
+  })
 
   const port = process.env.PORT == null ? 3000 : Number.parseInt(process.env.PORT)
   app.listen({ port, host: '0.0.0.0' }, (err, address) => {
